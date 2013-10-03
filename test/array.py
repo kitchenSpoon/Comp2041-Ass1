d78116d45a3fbcd6fc3abffd792b0aedb437da54
KithenSpoon <hljw4@hotmail.com>
2013-10-04 00:49:29 +1000

    Added push,pop,shift,unshift and reverse

--- test/array.py
+++ test/array.py
@@ -3,24 +3,17 @@ import sys
 
 array = [1,2,'b']
 
-#push @array,5;
+array.append(5)
+print  array
 
+array.pop()
 
 print  array
 
-#pop @array;
-
-
+array.pop(0)
 print  array
 
-#shift @array;
-
-
-print  array
-
-#unshift @array,'Hi';
-
-
+array.insert(0,'Hi')
 print  array
 
 rArray = array[::-1]
