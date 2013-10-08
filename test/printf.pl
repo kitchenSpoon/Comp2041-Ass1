489d13462c212a8dd49f92a046a67513bb20e079
KithenSpoon <hljw4@hotmail.com>
2013-10-09 01:32:41 +1100

    Add printf, proper commit with all the relevant files

---
+++ test/printf.pl
@@ -0,0 +1,7 @@
+#!/usr/local/bin/perl -w
+
+$name = "jack";
+printf "%s Is Awesome\n",$name;
+printf "%d\n", 3.1415126;
+printf "The cost is %6.2f\n",499;
+printf "%04d\n", 20;
---
+++ test/printf.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2.7 -u
+import sys
+import re
+
+name = "jack"
+print "%s Is Awesomenot" % name 
+print "%d" % 3.1415126 
+print "The cost is %6.2f" % 499 
+print "%04d" % 20 
